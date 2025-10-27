fd3c63b4da8bc579983eae3f5068b5894578ddfa
bangjeonghan <aufkrh@naver.com>
2025-10-27 23:07:03 +0900

    portal_front 로그인 및 회원가입 페이지 까지 / auth 기본 프로젝트 세팅

---
+++ auth/auth/src/test/java/jh_platform/auth/AuthApplicationTests.java
@@ -0,0 +1,13 @@
+package jh_platform.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
